4e2136e3cde5cbbc915f8c9803e4e0bbaef3a452
Atuooo <atuooo@AtuooodeMacBook-Air.local>
2014-11-05 15:10:56 +0800

    Initial Commit

---
+++ EveMemory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EveMemory
+//
+//  Created by Atuooo on 14/11/5.
+//  Copyright (c) 2014年 Atuooo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
